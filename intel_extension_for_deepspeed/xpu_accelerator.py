37c47b942759237f9e07c80654d06ef9f2e9cde3
AGUL <mingzhi.liu@intel.com>
2022-11-16 11:08:58 +0800

    clean the interface of xpu_accelerator (#32)

--- intel_extension_for_deepspeed/xpu_accelerator.py
+++ intel_extension_for_deepspeed/xpu_accelerator.py
@@ -24,12 +24,6 @@ class XPU_Accelerator(DeepSpeedAccelerator):
 
     def device(self, device_index=None):
         return torch.xpu.device(device_index)
-    
-    def name(self):
-        return self._name
-    
-    def communication_backend_name(self):
-        return self._communication_backend_name
 
     def set_device(self, device_index):
         torch.xpu.set_device(device_index)
