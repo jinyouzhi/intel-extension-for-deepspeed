5db649a8c37f3d40d9468ad036645fe05b95ef7a
AGUL <mingzhi.liu@intel.com>
2022-11-25 15:09:55 +0800

    Mingzhil/fix jit (#29)
    * add jit build code.
    
    * support ipex latest lazy_init
    
    * fix bugs in jit_load (#28)
    
    * remove SYCL_KERNEL macro, move fused_adam_frontend.cpp to SYCL builder (#27)
    
    * remove SYCL_KERNEL macro, move fused_adam_frontend.cpp to SYCL builder
    
    * fix fused_adam_frontend.cpp path
    
    * fix bugs for jit load test
    
    * fix bug in builder.py
    
    * WA for latest ipex
    
    * remove nvcc parameters.
    
    * change header file for new compiler 0630
    
    * Support jit_load mode and no compiler prefix prebuild mode
    
    * modify sycl_kernel_path
    
    * fix return path
    
    * remove instance
    
    * fix path
    
    Co-authored-by: Han, Xu <xu.han@intel.com>
    Co-authored-by: Ma, Guokai <guokai.ma@gmail.com>
    Co-authored-by: Guo Yejun <yejun.guo@intel.com>

--- intel_extension_for_deepspeed/op_builder/builder.py
+++ intel_extension_for_deepspeed/op_builder/builder.py
@@ -2,6 +2,8 @@
 Copyright 2020 The Microsoft DeepSpeed Team
 """
 import os
+import time
+import importlib
 import shutil
 from pathlib import Path
 from deepspeed.ops.op_builder.builder import OpBuilder, TORCH_MAJOR, TORCH_MINOR
@@ -43,14 +45,91 @@ class SYCLOpBuilder(OpBuilder):
     def extra_ldflags(self):
         return ['-fPIC', '-Wl,-export-dynamic']
 
+    def load(self, verbose=True):
+        from deepspeed.git_version_info import installed_ops, torch_info
+        if installed_ops[self.name]:
+            return importlib.import_module(self.absolute_name())
+        else:
+            return self.jit_load(verbose)
+
+    def jit_load(self, verbose=True):
+        if not self.is_compatible(verbose):
+            raise RuntimeError(
+                f"Unable to JIT load the {self.name} op due to it not being compatible due to hardware/software issue. {self.error_log}"
+            )
+        try:
+            import ninja  # noqa: F401
+        except ImportError:
+            raise RuntimeError(
+                f"Unable to JIT load the {self.name} op due to ninja not being installed."
+            )
+
+        self.jit_mode = True
+        try:
+            from intel_extension_for_pytorch.xpu.cpp_extension import load
+        except ImportError:
+            from intel_extension_for_pytorch.xpu.utils import load
+
+        start_build = time.time()
+        #Recognize relative paths as absolute paths for jit load
+        
+        sources = [self.deepspeed_src_path(path) for path in self.sources()]
+        extra_include_paths = [
+            self.deepspeed_src_path(path) for path in self.include_paths()
+        ]
+
+        # Torch will try and apply whatever CCs are in the arch list at compile time,
+        # we have already set the intended targets ourselves we know that will be
+        # needed at runtime. This prevents CC collisions such as multiple __half
+        # implementations. Stash arch list to reset after build.
+        '''
+        torch_arch_list = None
+        if "TORCH_CUDA_ARCH_LIST" in os.environ:
+            torch_arch_list = os.environ.get("TORCH_CUDA_ARCH_LIST")
+            os.environ["TORCH_CUDA_ARCH_LIST"] = ""
+        '''
+
+        op_module = load(
+            name=self.name,
+            sources=self.strip_empty_entries(sources),
+            extra_include_paths=self.strip_empty_entries(extra_include_paths),
+            extra_cflags=self.strip_empty_entries(self.cxx_args()),
+            # extra_cuda_cflags=self.strip_empty_entries(self.nvcc_args()),
+            extra_ldflags=self.strip_empty_entries(self.extra_ldflags()),
+            verbose=verbose)
+        
+
+        build_duration = time.time() - start_build
+        if verbose:
+            print(f"Time to load {self.name} op: {build_duration} seconds")
+        '''
+        # Reset arch list so we are not silently removing it for other possible use cases
+        if torch_arch_list:
+            os.environ["TORCH_CUDA_ARCH_LIST"] = torch_arch_list
+        '''
+        return op_module
+
+
+
 def sycl_kernel_path(code_path):
+    
+    from .fused_adam import FusedAdamBuilder
     import intel_extension_for_pytorch
-    abs_path = os.path.join(Path(__file__).parent.absolute(), code_path)
-    rel_path = os.path.join("third-party", code_path)
-    print ("Copying SYCL kernel file from {} to {}".format(abs_path, rel_path))
-    os.makedirs(os.path.dirname(rel_path), exist_ok=True)
-    shutil.copyfile(abs_path, rel_path)
-    return rel_path
+    abs_source_path = os.path.join(Path(__file__).parent.absolute(), code_path)
+    rel_target_path = os.path.join("third-party", code_path)
+    
+    #jit_load mode require absolute path. Use abs path for copy
+    abs_target_path = FusedAdamBuilder().deepspeed_src_path(rel_target_path)
+    import filecmp
+    if(os.path.exists(abs_target_path) and filecmp.cmp(abs_target_path,abs_source_path) ):
+        print("skip copy, {} and {} are has the same content".format(abs_source_path,abs_target_path))
+        return os.path.join("deepspeed/ops",rel_target_path)
+    print ("Copying SYCL kernel file from {} to {}".format(abs_source_path, abs_target_path))
+    os.makedirs(os.path.dirname(abs_target_path), exist_ok=True)
+    shutil.copyfile(abs_source_path, abs_target_path)
+
+    #prebuild mode paths require relative to the setup.py directory. Use relative path
+    return rel_target_path
 
 def sycl_kernel_include(code_path):
     import intel_extension_for_pytorch
